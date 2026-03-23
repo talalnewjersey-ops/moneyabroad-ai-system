7b212c6804581b6e86847ea6d384fc17aacca837
talalnewjersey-ops <talalnewjersey@gmail.com>
2026-03-22 23:28:01 -0400

    Update daily_publisher.py

--- workflows/daily_publisher.py
+++ workflows/daily_publisher.py
@@ -3,6 +3,11 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from agents.content_agent import create_article
 from utils.wordpress import publish_post
 
